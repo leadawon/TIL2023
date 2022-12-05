c0cfacef454e5e2ea4989e9839ae482bd1a2aa74
Lee Dawon <dawon337@gmail.com>
2022-12-05 10:42:13 +0900

    plzplzplzplzplz

---
+++ do_it_django/venvs/mysite.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\USER\Desktop\leedawon\TIL_github\TIL2023\do_it_django\projects\mysite
+C:\Users\USER\Desktop\leedawon\TIL_github\TIL2023\do_it_django\venvs\mysite\Scripts\activate
+
