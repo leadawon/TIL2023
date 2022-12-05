ccc1769d3e91376857824f47c52932160ef76910
Lee Dawon <dawon337@gmail.com>
2022-12-05 14:41:47 +0900

    do_it_django removed

--- do_it_django/venvs/mysite.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\USER\Desktop\leedawon\TIL_github\TIL2023\do_it_django\projects\mysite
-C:\Users\USER\Desktop\leedawon\TIL_github\TIL2023\do_it_django\venvs\mysite\Scripts\activate
-
